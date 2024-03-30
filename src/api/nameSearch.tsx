b3ab08dacb7f393b4aed7ac9148515bebc92db0c
Vova <bereznikovvova1@yandex.ru>
2024-03-30 21:04:31 +0300

    [add] i`m add main root and vse.

--- src/api/nameSearch.tsx
+++ src/api/nameSearch.tsx
@@ -1,14 +1,31 @@
-import GetService from "../services/getService";
+import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
+const key = "XMNC75V-WMTMP7X-K9GTNHA-R9YK8VX";
+
 export const nameSearch = (name: string) => {
+  const options = {
+    method: "GET",
+    url: `https://api.kinopoisk.dev/v1.4/movie/search`,
+    params: { page: "1", limit: "10", query: name },
+    headers: { accept: "application/json", "X-API-KEY": key },
+  };
   return useQuery({
     queryKey: ["nameSearch", name],
-    queryFn: async () =>
-      GetService.searchData(
-        { page: "1", limit: "10", query: name },
-        "v1.4/movie/search",
-      ),
+    queryFn: () => axios.request(options),
+    select: (data) => data.data,
+  });
+};
+export const randomMovie = () => {
+  const options = {
+    method: "GET",
+    url: "https://api.kinopoisk.dev/v1.4/movie/random",
+    params: { "rating.kp": "7.2-10" },
+    headers: { accept: "application/json", "X-API-KEY": key },
+  };
+  return useQuery({
+    queryKey: ["randomMovie"],
+    queryFn: () => axios.request(options),
     select: (data) => data.data,
   });
 };
--- src/rootes/root/index.tsx
+++ src/rootes/root/index.tsx
@@ -2,11 +2,15 @@ import { ChangeEvent, useState } from "react";
 import { useMainStore } from "../../store";
 import "./style.css";
 import { Outlet, useNavigate, useParams } from "react-router-dom";
+import { randomMovie } from "../../api/nameSearch";
+
 export const Root = () => {
   const [value, setValue] = useState("");
   const myNavigator = useNavigate();
   const params = useParams();
   const { isThemeDark, toggleTheme } = useMainStore();
+  const { data, isLoading, isError, error } = randomMovie();
+  console.log(data);
   return (
     <div className={`root-cont ${isThemeDark ? "dark-theme" : "light-theme"}`}>
       <header className="top_panel">
@@ -43,8 +47,47 @@ export const Root = () => {
 
         <div>Menu</div>
       </header>
+      <img
+        style={{ left: "0" }}
+        src={
+          isThemeDark
+            ? "https://img.razrisyika.ru/kart/85/1200/339801-kinoteatr-34.jpg"
+            : "https://avatars.mds.yandex.net/get-altay/492546/2a0000015e5335bad431c2c5776e07747126/XXL_height"
+        }
+        className="backgroundMain"
+      />
+      <img
+        style={{ right: "0" }}
+        src={
+          isThemeDark
+            ? "https://img.razrisyika.ru/kart/85/1200/339801-kinoteatr-34.jpg"
+            : "https://avatars.mds.yandex.net/get-altay/492546/2a0000015e5335bad431c2c5776e07747126/XXL_height"
+        }
+        className="backgroundMain"
+      />
       <main>
-        main
+        {isLoading ? (
+          "Loading..."
+        ) : data ? (
+          <div
+            className="movieContainer"
+            onClick={() => myNavigator("/" + data.name)}
+          >
+            <div className="posterContainer">
+              <img src={data.poster.url} />
+              <h5>{data.ageRating ? data.ageRating : "0"}+</h5>
+              <h3>{data.rating.kp}</h3>
+            </div>
+            <h2>Название: {data.name ? data.name : data.alternativeName}</h2>
+            <h3>Год: {data.year}</h3>
+            <h4>
+              Жанры: {data.genres[0].name},{" "}
+              {data.genres[1] ? data.genres[1].name : null}
+            </h4>
+          </div>
+        ) : isError ? (
+          error.message
+        ) : null}
         <Outlet />
       </main>
       <footer>
@@ -54,7 +97,7 @@ export const Root = () => {
         <a href="https://music.yandex.ru/home">Музыка</a>
         <a href="https://afisha.yandex.ru">Афиша</a>
         <a href="https://bookmate.ru/">Букмейт</a>
-        <div>Проект компании Vova</div>
+        <div>Проект компании localhost</div>
       </footer>
     </div>
   );
